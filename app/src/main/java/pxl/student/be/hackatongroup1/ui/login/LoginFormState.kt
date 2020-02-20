f2073d13cfb1b8a86ecdb7bdb838aabc38087fa5
aaronnoels <noels.aaron@gmail.com>
2020-02-20 10:34:47 +0100

    Initial project

---
+++ app/src/main/java/pxl/student/be/hackatongroup1/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package pxl.student.be.hackatongroup1.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
